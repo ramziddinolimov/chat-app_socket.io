8359836102d97afef310f823f07d2d77e28a3fe4
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-10-14 14:25:47 +0500

    index js continued and client js started  file upload socket io

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,9 +1,14 @@
 const io = require('socket.io')(8000)
 
-const users = {}
+const users = {};
 
 io.on('connection', socket =>{
-    socket.on('user-joined', name =>{
-        
+    socket.on('new-user-joined', name =>{
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    })
+
+    socket.on('send', message =>{
+        socket.broadcast.emit('receive', {message: message, name: user[socket.id]})
     })
 })
